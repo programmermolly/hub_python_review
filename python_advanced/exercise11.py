31acc9aad89c8975807186677bf42f3ff758fe8d
programmerjx <jxkdy123@126.com>
2022-03-27 18:30:27 +0800

    python高级编程第四次提交

--- python_advanced/exercise11.py
+++ python_advanced/exercise11.py
@@ -2,13 +2,11 @@
 def my_enumerate(list_):
     count = 0
     for item in list_:
-        yield(count,item)
-        count+=1
+        yield (count, item)
+        count += 1
 
 
-
-
-if __name__=='__main__':
-    list_given=[1,2,4,7,9]
-    for index,item in my_enumerate(list_given):
-        print(index,item)
+if __name__ == '__main__':
+    list_given = [1, 2, 4, 7, 9]
+    for index, item in my_enumerate(list_given):
+        print(index, item)
