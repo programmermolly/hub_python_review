f2a086816d164fce751f72cb145409e3b801373b
programmerjx <jxkdy123@126.com>
2022-03-27 22:42:25 +0800

    python高级编程第四次提交

--- python_advanced/exercise13.py
+++ python_advanced/exercise13.py
@@ -2,14 +2,14 @@
 # 要求：2.获取列表中所有小数
 # list01=[3,'54',True,6,76,1.6,False,3.5]
 
-list01=[3,'54',True,6,76,1.6,False,3.5]
+list01 = [3, '54', True, 6, 76, 1.6, False, 3.5]
 
-generator1=(item for item in list01 if type(item)==str)
+generator1 = (item for item in list01 if type(item) == str)
 
 for item in generator1:
     print(item)
 
-generator2=(item for item in list01 if type(item)==float)
+generator2 = (item for item in list01 if type(item) == float)
 
 for item in generator2:
     print(item)
